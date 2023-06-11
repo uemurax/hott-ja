a9f0f1db3862111ed294efbf523a16efe6849656
Taichi Uemura <t.uemura00@gmail.com>
2023-06-11 21:19:53 +0200

    update section ref

--- src-1/config.rkt
+++ src-1/config.rkt
@@ -1,15 +1,24 @@
 #lang at-exp typed/racket
 
 (require (prefix-in latex: morg/latex/config)
-         morg/markup/tex)
+         morg/markup/tex
+         morg/markup/string)
 
 (provide latex-config)
+
+(define (latex-config:make-section-ref [depth : Natural] [num : StringTree])
+  (define x : String
+    (if (depth . <= . 1)
+        "章"
+        "節"))
+  @text-tex%{@|num|@|x|})
 
 (define latex-config
   (struct-copy latex:user-config latex:default-config
     [front-matter @text-tex%{@macro%{frontmatter}}]
     [main-matter @text-tex%{@macro%{mainmatter}}]
     [back-matter @text-tex%{@macro%{backmatter}}]
+    [make-section-ref latex-config:make-section-ref]
     [section-macros '("chapter" "section" "subsection" "subsubsection" "paragraph")]
     [section-macro-fallback "subparagraph"]
     [class "ltjsbook"]
