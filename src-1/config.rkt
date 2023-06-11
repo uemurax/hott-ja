17be48ed58ee0c1447ca3d95cc0f221d9842e27a
Taichi Uemura <t.uemura00@gmail.com>
2023-06-11 21:13:03 +0200

    update config

--- src-1/config.rkt
+++ src-1/config.rkt
@@ -1,11 +1,15 @@
 #lang at-exp typed/racket
 
-(require (prefix-in latex: morg/latex/config))
+(require (prefix-in latex: morg/latex/config)
+         morg/markup/tex)
 
 (provide latex-config)
 
 (define latex-config
   (struct-copy latex:user-config latex:default-config
+    [front-matter @text-tex%{@macro%{frontmatter}}]
+    [main-matter @text-tex%{@macro%{mainmatter}}]
+    [back-matter @text-tex%{@macro%{backmatter}}]
     [section-macros '("chapter" "section" "subsection" "subsubsection" "paragraph")]
     [section-macro-fallback "subparagraph"]
     [class "ltjsbook"]
