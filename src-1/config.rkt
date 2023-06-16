3cfde521e0058985752db96de0651a42dd918df9
Taichi Uemura <t.uemura00@gmail.com>
2023-06-16 12:54:46 +0200

    update config

--- src-1/config.rkt
+++ src-1/config.rkt
@@ -1,12 +1,11 @@
 #lang at-exp typed/racket
 
 (require (prefix-in latex: morg/latex/config)
-         morg/markup/tex
-         morg/markup/string)
+         morg/markup/tex)
 
 (provide latex-config)
 
-(define (latex-config:make-section-ref [depth : Natural] [num : StringTree])
+(define (latex-config:make-section-ref [depth : Natural] [num : String])
   (define x : String
     (if (depth . <= . 1)
         "章"
@@ -14,7 +13,7 @@
   @text-tex%{@|num|@|x|})
 
 (define latex-config
-  (struct-copy latex:user-config latex:default-config
+  (struct-copy latex:config latex:default-config
     [front-matter @text-tex%{@macro%{frontmatter}}]
     [main-matter @text-tex%{@macro%{mainmatter}}]
     [back-matter @text-tex%{@macro%{backmatter}}]
