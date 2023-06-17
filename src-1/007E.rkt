1027d6d17d88fcabb580d117a8316050bc87b728
Taichi Uemura <t.uemura00@gmail.com>
2023-06-17 20:59:03 +0200

    add van_den_Berg-Garner-2011-0000

--- src-1/007E.rkt
+++ src-1/007E.rkt
@@ -3,6 +3,7 @@
 (require "lib/markup.rkt")
 
 (require "HoTT-Book.rkt"
+         "van_den_Berg-Garner-2011-0000.rkt"
          "Lumsdaine-2010-0000.rkt")
 
 (provide part:007E)
@@ -13,6 +14,7 @@
     #:title @%{参考文献}
     part:HoTT-Book
     part:Lumsdaine-2010-0000
+    part:van_den_Berg-Garner-2011-0000
   ])
 
 (module+ main
---
+++ src-1/van_den_Berg-Garner-2011-0000.rkt
@@ -0,0 +1,26 @@
+#lang at-exp typed/racket
+
+(require "lib/markup.rkt"
+         (prefix-in b: "lib/bibliography.rkt"))
+
+(provide part:van_den_Berg-Garner-2011-0000)
+
+(define part:van_den_Berg-Garner-2011-0000
+  @b:bibliography[
+    #:id "van_den_Berg-Garner-2011-0000"
+    @b:article[
+      #:author @list[@%{Benno van den Berg} @%{Richard Garner}]
+      #:title @%{Types are weak ω-groupoids}
+      #:journal-title @%{Proceedings of the London Mathematical Society}
+      #:date (date 2011)
+      #:volume @%{102}
+      #:number @%{2}
+      #:pages @%{370--394}
+      #:doi "10.1112/plms/pdq026"
+	  #:eprint @b:eprint["0812.0298v2"]
+    ]
+  ])
+
+(module+ main
+  (require morg/text)
+  (display (->text part:van_den_Berg-Garner-2011-0000)))
