b2e82c4e54308b12ecb3165be2eb3d94368ac356
Taichi Uemura <t.uemura00@gmail.com>
2023-07-07 21:08:43 +0200

    add 005O

--- src-1/lib/math/functor.rkt
+++ src-1/lib/math/functor.rkt
@@ -3,6 +3,9 @@
 (require "util.rkt")
 
 (provide functor
+         is-fully-faithful
+         is-ess-surj
+         is-weak-cat-equiv
          functor:obj
          functor:map)
 
@@ -10,3 +13,9 @@
 (define functor (make-fun functor/symb))
 (define functor:obj (const "obj"))
 (define functor:map (const "map"))
+(define is-fully-faithful/symb (const "IsFF"))
+(define is-fully-faithful (make-fun is-fully-faithful/symb))
+(define is-ess-surj/symb (const "IsEssSurj"))
+(define is-ess-surj (make-fun is-ess-surj/symb))
+(define is-weak-cat-equiv/symb (const "IsWCatEquiv"))
+(define is-weak-cat-equiv (make-fun is-weak-cat-equiv/symb))
