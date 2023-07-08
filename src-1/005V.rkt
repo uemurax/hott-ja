361c255c4f4975aad2b2071533ffebfd918bb3a0
Taichi Uemura <t.uemura00@gmail.com>
2023-07-08 13:37:38 +0200

    add 005V

---
+++ src-1/005V.rkt
@@ -0,0 +1,22 @@
+#lang at-exp typed/racket
+
+(require "lib/markup.rkt"
+         "lib/markup/definition.rkt"
+         "lib/math.rkt"
+         "lib/math/n-type.rkt"
+         "lib/math/connected.rkt")
+
+(provide-part (id)
+  @definition[
+    #:id id
+    #:indexes @list[
+      @index[#:key "nれんけつかんすう"]{@${n}連結関数}
+    ]
+    @paragraph{
+      @${i}を階数、@($ ("A" . elem-of . @universe{i}))と@($ ("B" . elem-of . @universe{i}))を型、@($ ("f" . elem-of . ("A" . fun-type . "B")))を関数、@($ ("n" . elem-of . trunc-level))を要素とする。型@($ ((is-connected-map (seq "n" "f")) . elem-of . @universe{i}))を@($ (d-fun-type ("y" . elem-of . "B") (is-connected (seq "n" (fiber (seq "f" "y"))))))と定義する。@($ (is-connected-map (seq "n" "f")))の要素がある時、@${f}は@dfn{@${n}連結}であると言う。
+    }
+  ])
+
+(module+ main
+  (require morg/text)
+  (preview))
--- src-1/lib/math/connected.rkt
+++ src-1/lib/math/connected.rkt
@@ -2,7 +2,10 @@
 
 (require "util.rkt")
 
-(provide is-connected)
+(provide is-connected
+         is-connected-map)
 
 (define is-connected/symb (const "IsConnected"))
 (define is-connected (make-fun is-connected/symb))
+(define is-connected-map/symb (const "IsConnMap"))
+(define is-connected-map (make-fun is-connected-map/symb))
