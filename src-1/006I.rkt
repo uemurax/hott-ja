d446ca2668dd4dc56475e0d91aedfb2d6aea8256
Taichi Uemura <t.uemura00@gmail.com>
2023-07-08 20:24:20 +0200

    add 006I

---
+++ src-1/006I.rkt
@@ -0,0 +1,27 @@
+#lang at-exp typed/racket
+
+(require "lib/markup.rkt"
+         "lib/markup/lemma.rkt"
+         "lib/math.rkt"
+         "lib/math/cat.rkt")
+
+(provide-part (id)
+  @lemma[
+    #:id id
+    @paragraph{
+      関数外延性を仮定する。@${i}を階数、@($ ("A" . elem-of . @set-cat{i}))と@($ ("B" . elem-of . @set-cat{i}))を対象、@($ ("f" . elem-of . (cat:map . fun-apply . (seq "A" "B"))))を射とする。次は論理的に同値である。
+      @ordered-list[
+        @list-item[#:id "0000"]{@${f}は@${@set-cat{i}}の射として同型である。}
+        @list-item[#:id "0001"]{@${f}は関数として同値である。}
+      ]
+    }
+    #:proof @proof[
+      @paragraph{
+        @ref["004K"]による。
+      }
+    ]
+  ])
+
+(module+ main
+  (require morg/text)
+  (preview))
