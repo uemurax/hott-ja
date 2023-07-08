4fff642bfcab40babc25a621d249f3b282b62853
Taichi Uemura <t.uemura00@gmail.com>
2023-07-08 14:07:32 +0200

    add 0061

---
+++ src-1/0061.rkt
@@ -0,0 +1,19 @@
+#lang at-exp typed/racket
+
+(require "lib/markup.rkt"
+         "lib/markup/exercise.rkt"
+         "lib/math.rkt"
+         "lib/math/cat.rkt"
+         "lib/math/functor.rkt")
+
+(provide-part (id)
+  @exercise[
+    #:id id
+    @paragraph{
+      @${i}を階数、@($ ("C" . elem-of . @precat{i}))と@($ ("D" . elem-of . @precat{i}))を前圏、@($ ("F" . elem-of . (functor (seq "C" "D"))))を関手、@($ (("x" . _ . "1") . elem-of . "C"))と@($ (("x" . _ . "2") . elem-of . "C"))を対象、@($ ("f" . elem-of . (cat:map . fun-apply . (seq ("x" . _ . "1") ("x" . _ . "2")))))を射とする。@${F}は充満忠実であると仮定する。@($ ("F" . fun-apply . "f"))が同型ならば@${f}は同型であることを示せ。
+    }
+  ])
+
+(module+ main
+  (require morg/text)
+  (preview))
