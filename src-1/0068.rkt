bb0da81382685bb28d1d347b6d1f082bec847239
Taichi Uemura <t.uemura00@gmail.com>
2023-07-08 17:37:20 +0200

    add 0068

---
+++ src-1/0068.rkt
@@ -0,0 +1,25 @@
+#lang at-exp typed/racket
+
+(require "lib/markup.rkt"
+         "lib/markup/proposition.rkt"
+         "lib/math.rkt"
+         "lib/math/cat.rkt"
+         "lib/math/functor.rkt"
+         "lib/math/nat-trans.rkt"
+         "lib/math/logic.rkt")
+
+(provide-part (id)
+  @proposition[
+    #:id id
+    @paragraph{
+      関数外延性を仮定する。@${i}を階数、@($ ("C" . elem-of . @precat{i}))と@($ ("D" . elem-of . @precat{i}))を前圏、@($ ("F" . elem-of . (functor (seq "C" "D"))))と@($ ("G" . elem-of . (functor (seq "C" "D"))))を関手、@($ ("s" . elem-of . (nat-trans (seq "F" "G"))))と@($ ("t" . elem-of . (nat-trans (seq "F" "G"))))を自然変換とすると、同値
+      @disp{
+        @($ (("s" . id-type . "t") . equiv . (forall ("x" . elem-of . "C") (("s" . fun-apply . "x") . id-type . ("t" . fun-apply . "x")))))
+      }
+      を構成できる。
+    }
+  ])
+
+(module+ main
+  (require morg/text)
+  (preview))
