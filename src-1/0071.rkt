3bc10a7bd8eaae2ac7c8b45ef3d55046291e47f5
Taichi Uemura <t.uemura00@gmail.com>
2023-07-09 11:03:01 +0200

    add 0071

---
+++ src-1/0071.rkt
@@ -0,0 +1,29 @@
+#lang at-exp typed/racket
+
+(require "lib/markup.rkt"
+         "lib/markup/lemma.rkt"
+         "lib/math.rkt"
+         "lib/math/cat.rkt"
+         "lib/math/presheaf.rkt"
+         "lib/math/functor.rkt")
+
+(provide-part (id)
+  @lemma[
+    #:id id
+    @paragraph{
+      関数外延性を仮定する。@${i}を階数、@($ ("C" . elem-of . @precat{i}))を前圏、@($ ("A" . elem-of . @presheaf{C}))を前層とすると、同値
+      @disp{
+        @($ (@is-repr-psh{A} . equiv . (iso-fiber (seq @yoneda{C} "A"))))
+      }
+      を構成できる。
+    }
+    #:proof @proof[
+      @paragraph{
+        @ref["006T"]と@ref["0072"]から従う。
+      }
+    ]
+  ])
+
+(module+ main
+  (require morg/text)
+  (preview))
