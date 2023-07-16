4ac26f9fd4262f8eaa3d636861eadd6b5c186eaa
Taichi Uemura <t.uemura00@gmail.com>
2023-07-16 22:30:50 +0200

    changes in bibliography

--- src/lib/bibliography.rkt
+++ src/lib/bibliography.rkt
@@ -1,10 +1,8 @@
 #lang at-exp typed/racket
 
-(require (only-in morg/markup %)
-          morg/bibliography)
+(require  morg/bibliography)
 
 (provide (except-out (all-from-out morg/bibliography) bibliography)
          (rename-out [bib bibliography]))
 
-(define bib
-  (bibliography/curried #:header @%{文献}))
+(define bib @make-bibliography{文献})
