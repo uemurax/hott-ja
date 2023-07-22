8ce6597dc138f7edf40e17808c2b8e8fd330b201
Taichi Uemura <t.uemura00@gmail.com>
2023-07-22 16:45:17 +0200

    typo

--- src/006U.rkt
+++ src/006U.rkt
@@ -12,6 +12,6 @@
 @notation[
   #:id (current-id)
   @paragraph{
-    関数外延性を仮定する。@(math i)を階数、@(math (C . elem-of . (precat . $ . i)))を前圏、@(math (x . elem-of . C))を対象とする。要素@(math ((yoneda-gen . $ . x) . elem-of . (((yoneda . $ . C) . $ . x) . $ . x)))を@(math (cat:id . $ . x))と定義する。
+    関数外延性を仮定する。@(math i)を階数、@(math (C . elem-of . (precat . $ . i)))を前圏、@(math (x . elem-of . C))を対象とする。要素@(math ((yoneda-gen . $ . x) . elem-of . (((yoneda . $ . C) . $ . x) . $ . x)))を@(math (cat:id . $i . x))と定義する。
   }
 ]
