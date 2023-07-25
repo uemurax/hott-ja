7642b83b12e06fbd4e12ed5ccadf35d8f07a4d45
Taichi Uemura <t.uemura00@gmail.com>
2023-07-25 21:52:02 +0200

    minor change

--- src/003V.rkt
+++ src/003V.rkt
@@ -16,6 +16,6 @@
   @paragraph{
     関数外延性と一価性を仮定する。@(math i)を階数、@(math (A . elem-of . (span . $ . i)))をスパン、@(math (B . elem-of . (span-over . $ . A)))を@(math A)上のスパン、@(math (C . elem-of . (span/cocone . $ . A)))を余錐とする。@(math B)がカルテシアンで、@(math C)が普遍余錐であると仮定すると、型@disp{
       @(math (prop-compr (D . elem-of . (span/cocone-over . $* . B C)) (span/cocone-is-cartesian . $ . D)))
-    }は可縮である。さらに、任意の@(math (D . elem-of . (span/cocone-over . $* . B C)))に対して、@(math D)がカルテシアンならば@(math ((total-span/cocone . $ . D) . elem-of . (span/cocone . $ . (total-span . $ . B))))は普遍余錐である。
+    }は可縮である。さらに、一意に存在するカルテシアンな@(math (D . elem-of . (span/cocone-over . $* . B C)))に対して、@(math ((total-span/cocone . $ . D) . elem-of . (span/cocone . $ . (total-span . $ . B))))は普遍余錐である。
   }
 ]
